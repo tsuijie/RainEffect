bfdb8eb7bebb3c45002191e50c66f6a4cf7c5940
root <zxsoft>
2020-08-30 21:15:20 +0800

    Updates

---
+++ precache-manifest.463704381410bfb181ff768b5f0f03be.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a8c642d85474bca8312a9288f62f2e4",
+    "url": "/RainEffect/index.html"
+  },
+  {
+    "revision": "a985f7d0bc17818e51e2",
+    "url": "/RainEffect/static/css/main.55932be9.chunk.css"
+  },
+  {
+    "revision": "94ac894de54cd5e104ad",
+    "url": "/RainEffect/static/js/2.50db5c07.chunk.js"
+  },
+  {
+    "revision": "89a6b555af4184298a5af61b72cf43aa",
+    "url": "/RainEffect/static/js/2.50db5c07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a985f7d0bc17818e51e2",
+    "url": "/RainEffect/static/js/main.235ba0e1.chunk.js"
+  },
+  {
+    "revision": "cdc2777005caef5da690",
+    "url": "/RainEffect/static/js/runtime-main.5a450691.js"
+  },
+  {
+    "revision": "1a5d08c1afedf80bd30e97a871f79ae1",
+    "url": "/RainEffect/static/media/texture-rain-bg.1a5d08c1.png"
+  },
+  {
+    "revision": "e8973e275fe88c83e0ee22edc29e0b38",
+    "url": "/RainEffect/static/media/texture-rain-fg.e8973e27.png"
+  }
+]);
